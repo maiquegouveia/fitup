d068da285123acb2f4c614142184fa84e9a99aa5
maiquegouveia <maiquebusiness@gmail.com>
2023-05-02 15:18:59 -0300

    Test

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Page 1</Text>
+    </View>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({});
