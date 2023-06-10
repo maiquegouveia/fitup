ef6cc094b5737a916bef3a41de55b058864aa47f
maiquegouveia <maiquebusiness@gmail.com>
2023-06-10 00:57:53 -0300

    Working on the EditDish screen, adding/removing food from dish functionality

--- src/screens/Dish/EditDish.js
+++ src/screens/Dish/EditDish.js
@@ -1,23 +1,32 @@
 import { StyleSheet, Text, View, ScrollView, RefreshControl, TouchableOpacity } from 'react-native';
 import { Button, ActivityIndicator } from 'react-native-paper';
 import { useContext, useState, useEffect, useCallback } from 'react';
-import { ThemeContext } from '../../../contexts/ThemeProvider';
-import FoodListItem from './components/FoodListItem';
 import { NativeBaseProvider } from 'native-base';
-import getDishItens from '../../../utilities/Dish/getDishItens';
 import { useIsFocused } from '@react-navigation/native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import getDishItens from '../../../utilities/Dish/getDishItens';
+import FoodListItem from './components/FoodListItem';
+import FoodSelect from './components/FoodSelect';
+import { ThemeContext } from '../../../contexts/ThemeProvider';
+import AppContext from '../../../AppContext';
 
 const EditDish = ({ route, navigation }) => {
   const { theme } = useContext(ThemeContext);
-  const { params } = route;
+  const { params } = useContext(AppContext);
   const [isLoading, setIsLoading] = useState(true);
   const [foodAddedList, setFoodAddedList] = useState([]);
-  const isFocused = useIsFocused();
+  const [foodList, setFoodList] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const isFocused = useIsFocused();
 
   const getData = async () => {
-    const data = await getDishItens(params.dishId);
+    const data = await getDishItens(route.params.dishId);
+    const itensId = data.map((food) => food.alimento_id);
+    const cleanFavoriteList = params.favoriteList.filter((food) => {
+      if (!itensId.includes(food.alimento_id)) {
+        return food;
+      }
+    });
 
     setFoodAddedList(
       data.map((food) => {
@@ -31,12 +40,26 @@ const EditDish = ({ route, navigation }) => {
         };
       })
     );
+
+    setFoodList(
+      cleanFavoriteList.map((food) => {
+        return {
+          foodName: food.nome,
+          foodId: food.alimento_id,
+          amount: 100,
+          carbo: food.carboidrato,
+          kcal: food.kcal,
+          protein: food.proteina,
+        };
+      })
+    );
     setIsLoading(false);
   };
 
   const resetStates = () => {
     setIsLoading(true);
     setFoodAddedList([]);
+    setFoodList([]);
   };
 
   const onRefresh = useCallback(() => {
@@ -69,6 +92,30 @@ const EditDish = ({ route, navigation }) => {
     return `${total.toFixed(2)}g`;
   };
 
+  const handleOnSave = () => {
+    console.log(foodAddedList);
+  };
+
+  const onSelectFood = (foodId) => {
+    const updatedFoodList = [...foodList];
+    const foodAddedIndex = updatedFoodList.findIndex((food) => food.foodId === foodId);
+    const updatedFoodAddedList = [...foodAddedList];
+    updatedFoodAddedList.push(updatedFoodList[foodAddedIndex]);
+    updatedFoodList.splice(foodAddedIndex, 1);
+    setFoodAddedList(updatedFoodAddedList);
+    setFoodList(updatedFoodList);
+  };
+
+  const onRemove = (foodId) => {
+    const updatedFoodList = [...foodList];
+    const updatedFoodAddedList = [...foodAddedList];
+    const foodIndex = updatedFoodAddedList.findIndex((food) => food.foodId === foodId);
+    updatedFoodList.push(updatedFoodAddedList[foodIndex]);
+    updatedFoodAddedList.splice(foodIndex, 1);
+    setFoodAddedList(updatedFoodAddedList);
+    setFoodList(updatedFoodList);
+  };
+
   return (
     <NativeBaseProvider>
       <ScrollView
@@ -85,7 +132,7 @@ const EditDish = ({ route, navigation }) => {
             }}
           >
             <View style={{ width: '80%' }}>
-              <Text style={[styles.dishName]}>{params.dishName}</Text>
+              <Text style={[styles.dishName]}>{route.params.dishName}</Text>
             </View>
             <TouchableOpacity onPress={() => {}} activeOpacity={0.7}>
               <FontAwesome5 name="trash" size={20} color="red" />
@@ -94,7 +141,7 @@ const EditDish = ({ route, navigation }) => {
           <View style={{ backgroundColor: '#CD853F', marginVertical: 10, padding: 10, borderRadius: 5 }}>
             <View style={{ flexDirection: 'row' }}>
               <Text style={{ fontWeight: 'bold', color: theme.fontColor.text }}>Categoria: </Text>
-              <Text style={{ color: theme.fontColor.text }}>{params.dishCategory}</Text>
+              <Text style={{ color: theme.fontColor.text }}>{route.params.dishCategory}</Text>
             </View>
             <View style={{ flexDirection: 'row' }}>
               <Text style={{ fontWeight: 'bold', color: theme.fontColor.text }}>Calorias: </Text>
@@ -109,8 +156,10 @@ const EditDish = ({ route, navigation }) => {
               <Text style={{ color: theme.fontColor.text }}>{getTotalProtein()}</Text>
             </View>
           </View>
-          <View style={{ maxHeight: '60%', borderRadius: 5, backgroundColor: '#256D1B' }}>
-            {foodAddedList.length > 0 && (
+          {!isLoading && <FoodSelect foodList={foodList} onSelectFood={onSelectFood} />}
+
+          <View style={{ marginTop: 10, maxHeight: '50%', borderRadius: 5, backgroundColor: '#256D1B' }}>
+            {!isLoading && (
               <View style={{ alignItems: 'center', padding: 10 }}>
                 <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}>
                   Alimentos ({foodAddedList.length})
@@ -126,7 +175,7 @@ const EditDish = ({ route, navigation }) => {
                     key={index}
                     foodName={food.foodName}
                     amount={food.amount}
-                    onRemove={() => {}}
+                    onRemove={onRemove}
                     foodId={food.foodId}
                     style={{ marginBottom: index === foodAddedList.length - 1 ? 0 : 10 }}
                     foodKcal={food.kcal}
@@ -140,7 +189,7 @@ const EditDish = ({ route, navigation }) => {
           </View>
           {foodAddedList.length > 0 && (
             <View style={{ alignItems: 'center', marginTop: 10 }}>
-              <Button textColor="white" style={styles.button} onPress={() => {}}>
+              <Button textColor="white" style={styles.button} onPress={handleOnSave}>
                 Salvar
               </Button>
             </View>
